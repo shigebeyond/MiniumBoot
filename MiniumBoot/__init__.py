44abaa9a38dc1a3c849701cc70446bb08e6443a9
shijianhang <3076679995@qq.com>
2023-03-29 17:54:33 +0800

    升级依赖

--- MiniumBoot/__init__.py
+++ MiniumBoot/__init__.py
@@ -5,7 +5,7 @@ from .boot import Boot
 from .driver import MiniTestDriver
 
 __author__ = "shigebeyond"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __description__ = "MiniumBoot: make an easy way (yaml) to wechat miniprogram automation testing"
 
 __all__ = [
